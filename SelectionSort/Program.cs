3969a7d01bf2a60f9f9f0b840e6d0e331ab2f700
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-01-06 10:25:53 +0200

    Selection sort I

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] numbers = { 55, 20, 74, 46, 71, 63 };
+
+                Console.WriteLine("unsorted: " + String.Join(",", numbers));
+                SelectionSort.Sort(numbers);
+                Console.WriteLine("sorted: " + String.Join(",", numbers));
+                Console.WriteLine("*********************************************************");
+
+                numbers = new int[] { 55, 20, 74, 46, 71, 63 };
+
+#if RELEASE
+            BenchmarkRunner.Run<SortBenchmark>();
+#endif
+            }
+        }
+    }
+}
---
+++ SelectionSort/SelectionSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelectionSort
+{
+    public class SelectionSort
+    {
+        public static void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int idx_smallest = i; 
+
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[j] < array[idx_smallest])
+                    {
+                        idx_smallest = j;
+                    }
+
+                    var temp = array[i];
+                    array[i] = array[idx_smallest];
+                    array[idx_smallest] = temp;
+                }
+            }
+        }
+    }
+}
